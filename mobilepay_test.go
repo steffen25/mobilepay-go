985d50da8595fbeae1c55abc5ccd8fbded71f221
Steffen Thomsen <acbolic@gmail.com>
2021-07-28 23:32:57 +0200

    Add test case for private key with password option

--- mobilepay_test.go
+++ mobilepay_test.go
@@ -36,6 +36,39 @@ func TestNewBackendWithConfig(t *testing.T) {
 }
 
 func TestNewConfig(t *testing.T) {
+	const privWithPasswordPEM = `
+-----BEGIN RSA PRIVATE KEY-----
+Proc-Type: 4,ENCRYPTED
+DEK-Info: DES-EDE3-CBC,7487BB8910A3741B
+
+iL7m48mbFSIy1Y5xbXWwPTR07ufxu7o+myGUE+AdDeWWISkd5W6Gl44oX/jgXldS
+mL/ntUXoZzQz2WKEYLwssAtSTGF+QgSIMvV5faiP+pLYvWgk0oVr42po00CvADFL
+eDAJC7LgagYifS1l4EAK4MY8RGCHyJWEN5JAr0fc/Haa3WfWZ009kOWAp8MDuYxB
+hQlCKUmnUpXCp5c6jwbjlyinLj8XwzzjZ/rVRsY+t2Z0Vcd5qzR5BV8IJCqbG5Py
+z15/EFgMG2N2eYMsiEKgdXeKW2H5XIoWyun/3pBigWaDnTtiWSt9kz2MplqYfIT7
+F+0XE3gdDGalAeN3YwFPHCkxxBmcI+s6lQG9INmf2/gkJQ+MOZBVXKmGLv6Qis3l
+0eyUz1yZvNzf0zlcUBjiPulLF3peThHMEzhSsATfPomyg5NJ0X7ttd0ybnq+sPe4
+qg2OJ8qNhYrqnx7Xlvj61+B2NAZVHvIioma1FzqX8DxQYrnR5S6DJExDqvzNxEz6
+5VPQlH2Ig4hTvNzla84WgJ6USc/2SS4ehCReiNvfeNG9sPZKQnr/Ss8KPIYsKGcC
+Pz/vEqbWDmJwHb7KixCQKPt1EbD+/uf0YnhskOWM15YiFbYAOZKJ5rcbz2Zu66vg
+GAmqcBsHeFR3s/bObEzjxOmMfSr1vzvr4ActNJWVtfNKZNobSehZiMSHL54AXAZW
+Yj48pwTbf7b1sbF0FeCuwTFiYxM+yiZVO5ciYOfmo4HUg53PjknKpcKtEFSj02P1
+8JRBSb++V0IeMDyZLl12zgURDsvualbJMMBBR8emIpF13h0qdyah431gDhHGBnnC
+J5UDGq21/flFjzz0x/Okjwf7mPK5pcmF+uW7AxtHqws6m93yD5+RFmfZ8cb/8CL8
+jmsQslj+OIE64ykkRoJWpNBKyQjL3CnPnLmAB6TQKxegR94C7/hP1FvRW+W0AgZy
+g2QczKQU3KBQP18Ui1HTbkOUJT0Lsy4FnmJFCB/STPRo6NlJiATKHq/cqHWQUvZd
+d4oTMb1opKfs7AI9wiJBuskpGAECdRnVduml3dT4p//3BiP6K9ImWMSJeFpjFAFs
+AbBMKyitMs0Fyn9AJRPl23TKVQ3cYeSTxus4wLmx5ECSsHRV6g06nYjBp4GWEqSX
+RVclXF3zmy3b1+O5s2chJN6TrypzYSEYXJb1vvQLK0lNXqwxZAFV7Roi6xSG0fSY
+EAtdUifLonu43EkrLh55KEwkXdVV8xneUjh+TF8VgJKMnqDFfeHFdmN53YYh3n3F
+kpYSmVLRzQmLbH9dY+7kqvnsQm8y76vjug3p4IbEbHp/fNGf+gv7KDng1HyCl9A+
+Ow/Hlr0NqCAIhminScbRsZ4SgbRTRgGEYZXvyOtQa/uL6I8t2NR4W7ynispMs0QL
+RD61i3++bQXuTi4i8dg3yqIfe9S22NHSzZY/lAHAmmc3r5NrQ1TM1hsSxXawT5CU
+anWFjbH6YQ/QplkkAqZMpropWn6ZdNDg/+BUjukDs0HZrbdGy846WxQUvE7G2bAw
+IFQ1SymBZBtfnZXhfAXOHoWh017p6HsIkb2xmFrigMj7Jh10VVhdWg==
+-----END RSA PRIVATE KEY-----
+`
+
 	const privPEM = `
 -----BEGIN RSA PRIVATE KEY-----
 MIIEowIBAAKCAQEA4f5wg5l2hKsTeNem/V41fGnJm6gOdrj8ym3rFkEU/wT8RDtn
@@ -102,6 +135,28 @@ ODIRe1AuTyHceAbewn8b462yEWKARdpd9AjQW5SIVPfdsz5B6GlYQ5LdYKtznTuy
 			SubscriptionKey: "test",
 			AppSwitchSigner: &AppSwitchSigner{
 				PrivateKey: privKey,
+				PublicKey:  pubKey,
+				Signer:     cfg.AppSwitchSigner.Signer, // since the signer contains a slice we cannot compare its equality if we create one outside.
+			},
+		}
+		assert.Equal(t, expectedConfig, cfg)
+	}
+
+	{
+		pubKey, err := jwt.ParseRSAPublicKeyFromPEM([]byte(pubPEM))
+		assert.Nil(t, err)
+
+		privWithPasswordKey, err := jwt.ParseRSAPrivateKeyFromPEMWithPassword([]byte(privWithPasswordPEM), "password")
+		assert.Nil(t, err)
+
+		cfg, err := NewConfig("test", "test", OptionAppSwitchKeyPairWithPassword([]byte(pubPEM), []byte(privWithPasswordPEM), "password"))
+		assert.Nil(t, err)
+
+		expectedConfig := &Config{
+			MerchantID:      "test",
+			SubscriptionKey: "test",
+			AppSwitchSigner: &AppSwitchSigner{
+				PrivateKey: privWithPasswordKey,
 				PublicKey:  pubKey,
 				Signer:     cfg.AppSwitchSigner.Signer, // since the signer contains a slice we cannot compare its equality if we create one outside.
 			},
